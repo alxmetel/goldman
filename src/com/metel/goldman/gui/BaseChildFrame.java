89659e712139e2eb9c3d32e425cb43c6a2254d06
Alexander Metelchenko <alxmetel@gmail.com>
2016-02-04 12:50:14 +0200

    Initial project version. Create main frames

---
+++ src/com/metel/goldman/gui/BaseChildFrame.java
@@ -0,0 +1,48 @@
+package com.metel.goldman.gui;
+
+import java.awt.HeadlessException;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+import javax.swing.JFrame;
+
+/**
+ *
+ * @author Metel
+ */
+public abstract class BaseChildFrame extends JFrame {
+
+    public BaseChildFrame() {
+        setCloseOperation();
+    }
+    
+    private JFrame parentFrame;
+    
+    public JFrame parentFrame() {
+        return parentFrame;
+    }
+    
+    protected void showFrame(JFrame parent) {
+        this.parentFrame = parent;
+        parent.setVisible(false);
+        super.setVisible(true);
+    }
+    
+    protected void closeFrame() {
+        if (parentFrame==null) {
+            throw new IllegalArgumentException("parent frame must not be null!");
+        }
+        super.setVisible(false);
+        parentFrame.setVisible(true);  
+    }
+    
+    protected void setCloseOperation() {
+        super.setDefaultCloseOperation(JFrame.DO_NOTHING_ON_CLOSE);
+        
+        super.addWindowListener(new WindowAdapter() {
+            @Override
+            public void windowClosing(WindowEvent e) {
+                closeFrame();
+            }
+        });
+    }
+}
